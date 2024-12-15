3257af5e8362d3f20cb814d1254eae7aabe3a955
bachxuan10 <bachxuan10@gmail.com>
2024-12-15 22:56:40 +0700

    OCR

--- bao_cao_doanh_thu/Program.cs
+++ bao_cao_doanh_thu/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=OCR}/{action=Index}/{id?}");
 
 app.Run();
